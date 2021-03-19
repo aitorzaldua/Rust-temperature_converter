6de4560bb6ebd527eee4686db47e5847ff591fd6
Aitor Zaldua <aitor.zaldua@gmail.com>
2021-03-19 19:08:04 +0100

    190820210319

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,8 @@
 //This program converts the temperature between Fahrenheit and Celsius
-//Made with Rust by Aitor Zaldua on March 19th
+//Made with Rust by Aitor Zaldua on 2021 March 19th
 
 //(Faherenheit -32) x 5/9 = Celsius
 
-//Steps:
-//Ask to the user to insert the Fahrenheit
-//Create the variables F and C
-//Store the result
-//Print the result
 
 use std::io;
 
@@ -24,16 +19,45 @@ fn main () {
         .read_line(&mut user_input)
         .expect("Failed to read line");
 
-    println!("Your selecction: {}", user_input);
-
     let user_input: u32 = user_input.trim().parse().expect("Please type a number!");
 
     if user_input == 1 {
-        println! ("Lets convert F to C");
+
+        println! ("Please, insert the temperature in Fahrenheit");
+
+        let mut user_temperature = String::new();
+
+        io::stdin()
+            .read_line(&mut user_temperature)
+            .expect("Failed to read line");
+
+        let user_temperature: u32 = user_temperature.trim().parse().expect("Please type a number!");
+
+        let converter = (user_temperature -32) * 5/9;
+
+        println! ("They are {} Celsius", converter);
+
+
     }
+
     else if user_input == 2 {
-        println! ("Lets convert C to F");
+
+        println! ("Please, insert the temperature in Celsius");
+
+        let mut user_temperature = String::new();
+
+        io::stdin()
+            .read_line(&mut user_temperature)
+            .expect("Failed to read line");
+
+        let user_temperature: u32 = user_temperature.trim().parse().expect("Please type a number!");
+
+        let converter = (user_temperature * 9/5) + 32;
+
+        println! ("They are {} Fahrenheit", converter);
+
     }
+
     else  {
         println! ("Please, type 1 or 2");
     }
