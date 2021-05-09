b1a8ddd50c6e65ef4f2f4112171b1dc7a87a86ac
Aitor Zaldua <aitor.zaldua@gmail.com>
2021-05-09 10:20:14 +0200

    inicio del dia

---
+++ pirate_song/target/rls/debug/deps/pirate_song-3dc0b422f0797f81.d
@@ -0,0 +1,5 @@
+/mnt/c/3_Rust/variables_march/pirate_song/target/rls/debug/deps/pirate_song-3dc0b422f0797f81.rmeta: src/main.rs
+
+/mnt/c/3_Rust/variables_march/pirate_song/target/rls/debug/deps/pirate_song-3dc0b422f0797f81.d: src/main.rs
+
+src/main.rs:
---
+++ pirate_song/target/rls/debug/deps/pirate_song-ff45e726a60b2f5a.d
@@ -0,0 +1,5 @@
+/mnt/c/3_Rust/variables_march/pirate_song/target/rls/debug/deps/pirate_song-ff45e726a60b2f5a.rmeta: src/main.rs
+
+/mnt/c/3_Rust/variables_march/pirate_song/target/rls/debug/deps/pirate_song-ff45e726a60b2f5a.d: src/main.rs
+
+src/main.rs:
